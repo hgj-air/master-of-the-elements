81933d2ee582c59f361a328f0bfc79a5ed701423
Markus Kostrzewski <ciapa@ciapa.tech>
2019-10-13 10:30:33 +0200

    Implement basic OSD to show Lives and Score

---
+++ Assets/OSDScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OSDScript : MonoBehaviour
+{
+    private Text Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = $"Lives: {StateManager.Instance.Lives}        \nScore: {StateManager.Instance.Score.ToString("D8")}";
+    }
+}
--- Assets/StateManager.cs
+++ Assets/StateManager.cs
@@ -1,34 +1,34 @@
-    public class StateManager
+public class StateManager
+{
+    #region "Singleton"
+
+    private static StateManager _instance;
+
+    #endregion
+
+    #region Members
+
+    private int _lives;
+    private int _score;
+
+    #endregion
+
+    private StateManager()
     {
-        #region "Singleton"
-
-        private static StateManager _instance;
-
-        #endregion
-
-        #region Members
-
-        private int _lives;
-        private int _score;
-
-        #endregion
-
-        private StateManager()
-        {
-            _lives = 3;
-        }
-
-        public int Lives
-        {
-            get => _lives;
-            set => _lives = value;
-        }
-
-        public int Score
-        {
-            get => _score;
-            set => _score = value;
-        }
-
-        public static StateManager Instance => _instance ?? (_instance = new StateManager());
+        _lives = 3;
     }
+
+    public int Lives
+    {
+        get => _lives;
+        set => _lives = value;
+    }
+
+    public int Score
+    {
+        get => _score;
+        set => _score = value;
+    }
+
+    public static StateManager Instance => _instance ?? (_instance = new StateManager());
+}
